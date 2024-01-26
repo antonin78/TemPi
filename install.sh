b1c5c89713c8b9630f9e0002b294ae436629d3ce
Antonin <antonin.senay@hotmail.fr>
2024-01-26 14:17:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,11 +21,8 @@ apt install -y python apache2 php git unclutter
 # Suppression du fichier de base Apache2
 rm /var/www/html/index.html
 
-# Téléchargement des ressources
-git clone https://github.com/antonin78/TemPi.git 
-
 # Copie du fichier php dans la racine du serveur web
-cp TemPi/index.php /var/www/html
+cp index.php /var/www/html
 
 # Création d'un emplacement pour les logs de script
 mkdir -p ~/logs
