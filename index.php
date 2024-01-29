15e4d19296edaa23849f303ec2635c2258b1bfe5
Antonin <antonin.senay@hotmail.fr>
2024-01-29 14:32:38 +0100

    Add the last update option

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="refresh" content="20" />
+    <meta http-equiv="refresh" content="60" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TEMPO EDF - J et J+1</title>
@@ -34,11 +34,14 @@
         }
 
         .date-box {
+			display: flex;
+			flex-direction: column;
             position: absolute;
+			align-items: center;
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
-            padding: 10px;
+            padding: 5px;
             background-color: #000000; /* Noir */
             border-radius: 5px;
             color: #ffffff; /* Blanc */
@@ -46,6 +49,18 @@
             font-weight: bold;
             text-align: center;
         }
+	.footer {
+		position: fixed; /* Utilisez 'fixed' si vous souhaitez que le footer reste en bas même en faisant défiler la page, sinon utilisez 'absolute' */
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		text-align: center;
+		background-color: #000000; /* Couleur de fond du footer */
+		color: #CBCBCB; /* Couleur du texte du footer */
+		padding: 0.25%; /* Espacement interne du footer */
+		font-family: Arial, sans-serif;
+		font-style: italic;
+	}
     </style>
 </head>
 <body>
@@ -86,6 +101,29 @@
 	} else {
 		$var2 = '#242732';
 	}
+	
+	$nomFichier = './output.txt';
+
+	// Vérifiez si le fichier existe
+	if (file_exists($nomFichier)) {
+
+		// Obtenez le timestamp de la dernière modification
+		$timestampModification = filemtime($nomFichier);
+
+		// Créez un objet DateTime à partir du timestamp et définissez le fuseau horaire à UTC
+		$dateModification = new DateTime("@$timestampModification", new DateTimeZone('UTC'));
+
+		// Ajoutez la différence de fuseau horaire pour UTC+1
+		$dateModification->setTimezone(new DateTimeZone('Europe/Paris')); // Remplacez 'Europe/Paris' par le fuseau horaire de votre choix
+
+		// Formatez la date en une chaîne lisible
+		$dateModificationFormatee = $dateModification->format('d/m/Y à H:i');
+
+		// Affichez la date et l'heure de dernière modification en UTC+1
+		//echo "Dernière actualisation : $dateModificationFormatee";
+	} else {
+		echo "Pas de données disponibles";
+	}
 ?>
     <div class="half" style="background-color: <?php echo $var1; ?>;">
         <div class="date-box"><?php echo date('d/m/Y'); ?></div>
@@ -94,5 +132,6 @@
     <div class="half" style="background-color: <?php echo $var2; ?>;">
         <div class="date-box"><?php echo date('d/m/Y', strtotime('+1 day')); ?></div>
     </div>
+	<div class="footer"><?php echo "Dernière actualisation : $dateModificationFormatee"; ?></div>
 </body>
 </html>
